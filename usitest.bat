0e69ac208f99f11a14bff19cbe4c3f2bccef65de
bartland <bartland@users.noreply.github.com>
2020-07-13 16:42:59 +1000

    Added usitest.bat

--- usitest.bat
+++ usitest.bat
@@ -2,4 +2,4 @@
 set "proxy=-Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=8080"
 if "%1" == "" set "proxy="
 echo Running usi.gov.au.USITest ...
-C:\pf\java\jdk\bin\javaw.exe %proxy% -Dfile.encoding=Cp1252 -classpath ".\bin;.\lib\abrakm.jar;.\lib\auskey-dep-1.1.jar;.\lib\webservices-api.jar;.\lib\webservices-extra.jar;.\lib\webservices-rt.jar" usi.gov.au.USITest
+C:\pf\java\jdk\bin\java.exe %proxy% -Dfile.encoding=Cp1252 -classpath ".\bin;.\lib\abrakm.jar;.\lib\auskey-dep-1.1.jar;.\lib\webservices-api.jar;.\lib\webservices-extra.jar;.\lib\webservices-rt.jar" usi.gov.au.USITest
